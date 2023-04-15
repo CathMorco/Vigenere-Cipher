fb3a232fce50efa39b713170dc1dbb136a86b368
unknown <morco.cathleenjan.12192003@gmail.com>
2023-04-15 19:45:08 +0800

    Import pygame

---
+++ Vigenere Cipher Pygame Program.py
@@ -0,0 +1,37 @@
+#Morco, Cathleen Jan M., BSCPE 1-5 , 04/15/2023
+#A program that uses The Vigenère Cipher
+
+#Imports the pygame module
+import pygame
+
+#Sets the value for the width and height of the display screen
+
+#Creates the display screen
+
+#RGB example values
+
+#The rate of change in colors
+
+#The color directory & its values
+
+#Initialized values for functions
+
+#Draws the text
+
+#Creates the color change
+
+#Combines the color change and draw text into one function
+
+# Initializing pygame
+
+# Ask for user input
+
+# Make sure the keyword is long enough
+
+# Encrypt the message
+
+# Format the ciphertext into three lines
+
+#Informs the user of the output
+
+#Runs the program
